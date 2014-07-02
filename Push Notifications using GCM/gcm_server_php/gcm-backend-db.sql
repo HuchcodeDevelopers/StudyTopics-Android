0543766a895da602f02cb8588c277c33c14e1ced
Mark Tabamo <m.tabamo@huchcode.com>
2014-07-02 09:55:14 +0800

    Android study files - GCM, GFG Layout

---
+++ Push Notifications using GCM/gcm_server_php/gcm-backend-db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE  gcmdemo;
+
+USE gcmdemo;
+
+CREATE TABLE  `GCMDemo`.`tblRegistration` (
+`id` INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`registration_id` TEXT NOT NULL
+) ENGINE = INNODB;
+
+SELECT * FROM tblregistration;
